788d74ff37d5ccf08857328b707d73efc0257d90
Alex Zarauza <azarauza@nexusgeographics.com>
2024-02-02 12:07:18 +0100

    Fix: Project Name Fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'new-bases'`, () => {
+  it(`should have as title 'bases'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('new-bases');
+    expect(app.title).toEqual('bases');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('new-bases app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bases app is running!');
   });
 });
